eb54ff4914d858b0d270a9e556698a19f0db54b4
Vitaliy Obukh <Psiholog211@gmail.com>
2025-06-23 21:00:45 +0300

    fix env fail

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,13 +1,17 @@
-import NotesClient from "./Notes.client";
-import { fetchNotes } from "@/lib/api";
-import type { NotesResponse } from "@/types/note";
+import NotesClient from './Notes.client';
+import { fetchNotes } from '@/lib/api';
+import type { NotesResponse } from '@/types/note';
 
 export default async function NotesPage() {
   const perPage = 12;
   const initialPage = 1;
-  const initialSearch = "";
+  const initialSearch = '';
 
-  const initialData: NotesResponse = await fetchNotes(initialPage, initialSearch, perPage);
+  const initialData: NotesResponse = await fetchNotes(
+    initialPage,
+    initialSearch,
+    perPage
+  );
 
   return (
     <NotesClient
